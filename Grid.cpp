f14469cfbbc803e997fc4d16593a436732f6fcda
Fabio Suarez <fabiosuarez.o0@gmail.com>
2021-01-05 02:55:24 -0500

    idek what was wrong, this should've been simple

--- Grid.cpp
+++ Grid.cpp
@@ -18,23 +18,34 @@ void Grid::unset(unsigned short x, unsigned short y) {
 }
 
 void Grid::nextGeneration() {
+    std::vector<unsigned short> set;
+    std::vector<unsigned short> unset;
     for (unsigned short i = 0; i < this->height; i++) {
         for (unsigned short j = 0; j < this->width; j++) {
             unsigned char neighbors = 0;
-            for (unsigned char y = -1; y < 2; y++) {
-                for (unsigned char x = -1; x < 2; x++) {
-                    if (i + y >= 0 && i + y < this->height && j + x >= 0 && j + x < this->width && i + y != 0 && j + x != 0 && this->values[i + y][j + x]) {
+            for (char y = -1; y < 2; y++) {
+                for (char x = -1; x < 2; x++) {
+                    //printf("checked (%d, %d)\n", j + x, y + i);
+                    if (i + y >= 0 && i + y < this->height && j + x >= 0 && j + x < this->width && !(y == 0 || x == 0) && this->values[i + y][j + x]) {
                         neighbors++;
                     }
                 }
             }
-            printf("%d", neighbors);
+            //printf("(%d, %d) has %d neighbors\n", j, i, neighbors);
             if (this->values[i][j] && neighbors != 2 && neighbors != 3) {
-                this->unset(j, i);
+                unset.push_back(j);
+                unset.push_back(i);
             }
             else if (!this->values[i][j] && neighbors == 3) {
-                this->set(j, i);    
+                set.push_back(j);
+                set.push_back(i);
             }
         }
+    }
+    for (unsigned short i = 0; i < set.size(); i += 2) {
+        this->set(set[i], set[i + 1]);
+    }
+    for (unsigned short i = 0; i < unset.size(); i += 2) {
+        this->unset(unset[i], unset[i + 1]);
     }
 }
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argv, char** argc) {
     grid->set(30, 18);
     grid->set(30, 19);
     grid->set(30, 17);
-    unsigned int count = 0;
+    unsigned long long count = 0;
     while (1) {
         if (count == 6000) {
             count = 0;
@@ -15,6 +15,26 @@ int main(int argv, char** argc) {
         renderer->show();
         count++;
     }
+//    for (unsigned short i = 0; i < grid->height; i++) {
+//        for (unsigned short j = 0; j < grid->width; j++) {
+//            unsigned char neighbors = 0;
+//            for (char y = -1; y < 2; y++) {
+//                for (char x = -1; x < 2; x++) {
+//                    //printf("checked (%d, %d)\n", j + x, y + i);
+//                    if (i + y >= 0 && i + y < grid->height && j + x >= 0 && j + x < grid->width && !(y == 0 && x == 0) && grid->values[i + y][j + x]) {
+//                        neighbors++;
+//                    }
+//                }
+//            }
+//            printf("(%d, %d) has %d neighbors\n", j, i, neighbors);
+//            if (grid->values[i][j] && neighbors != 2 && neighbors != 3) {
+//                grid->unset(j, i);
+//            }
+//            else if (!grid->values[i][j] && neighbors == 3) {
+//                grid->set(j, i);    
+//            }
+//        }
+//    }
     return 0;
 }
 
